309496d0806cb6f9c3355837662df5e129ff2e0b
mamutmk5 <3045922+mamutmk5@users.noreply.github.com>
2024-03-20 16:34:52 +0100

    BC-6912 - code restructur (#1)

---
+++ src/test/java/de/svs/doido/mongo/MeResourceIT.java
@@ -0,0 +1,8 @@
+package svs.doido.mongo;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class MeResourceIT extends MeResourceTest {
+    // Execute the same tests but in packaged mode.
+}
