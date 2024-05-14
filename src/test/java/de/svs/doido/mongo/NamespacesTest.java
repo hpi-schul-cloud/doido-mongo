895663a26cc424861aad421625c86ee4b450c619
mamutmk5 <3045922+mamutmk5@users.noreply.github.com>
2024-05-14 12:55:21 +0200

    BC-7232 - test 1

---
+++ src/test/java/de/svs/doido/mongo/NamespacesTest.java
@@ -0,0 +1,34 @@
+package svs.doido.mongo;
+
+import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.is;
+
+@WithKubernetesTestServer
+@QuarkusTest
+class NamespacesTest {
+
+    @KubernetesTestServer
+    KubernetesServer mockServer;
+    @Inject
+    KubernetesClient client;
+    
+    @BeforeEach
+    public void before() {
+        final Pod pod1 = new PodBuilder().withNewMetadata().withName("pod1").withNamespace("test").and().build();
+        final Pod pod2 = new PodBuilder().withNewMetadata().withName("pod2").withNamespace("test").and().build();
+
+        // Set up Kubernetes so that our "pretend" pods are created
+        client.pods().resource(pod1).create();
+        client.pods().resource(pod2).create();
+    }
+
+    @Test
+    public void testInteractionWithAPIServer() {
+        RestAssured.when().get("/pod/test").then()
+                .body("size()", is(2));
+    }
+
+}
