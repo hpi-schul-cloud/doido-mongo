382a97428a50364631aa44d31fd7635400169a78
mamutmk5 <3045922+mamutmk5@users.noreply.github.com>
2024-05-17 10:35:11 +0200

    BC-7232 - itration

--- src/main/java/de/svs/doido/mongo/Namespaces.java
+++ src/main/java/de/svs/doido/mongo/Namespaces.java
@@ -11,6 +11,7 @@ import java.util.Collections;
 import java.util.List;
 import java.util.LinkedHashMap;
 import java.util.Set;
+import java.lang.Iterable;
 
 @Path("/namespace")
 public class Namespaces {
