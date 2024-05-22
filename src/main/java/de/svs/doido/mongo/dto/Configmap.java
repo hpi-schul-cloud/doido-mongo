5e2ebf1e00071ce4e6429c2c89b47ea9752eec3c
mamutmk5 <3045922+mamutmk5@users.noreply.github.com>
2024-05-22 17:24:17 +0200

    BC-7234 - fix test coverrage

--- src/main/java/de/svs/doido/mongo/dto/Configmap.java
+++ src/main/java/de/svs/doido/mongo/dto/Configmap.java
@@ -3,12 +3,10 @@ package svs.doido.mongo.dto;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
-import lombok.EqualsAndHashCode;
 import org.apache.commons.validator.routines.UrlValidator;
 
 
 @ToString
-@EqualsAndHashCode
 public class Configmap {
     @Getter
     @Setter
