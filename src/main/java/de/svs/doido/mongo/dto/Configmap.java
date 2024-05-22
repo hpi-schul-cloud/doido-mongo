bdf955f7dd417a5d2b49561f93745d9488957d3b
mamutmk5 <3045922+mamutmk5@users.noreply.github.com>
2024-05-22 15:41:07 +0200

    BC-7234 - test next

--- src/main/java/de/svs/doido/mongo/dto/Configmap.java
+++ src/main/java/de/svs/doido/mongo/dto/Configmap.java
@@ -14,7 +14,7 @@ public class Configmap {
     private String uri;
 
     public void setUri(String uriIn) {
-        UrlValidator urlValidator = new UrlValidator( ALLOW_ALL_SCHEMES );
+        UrlValidator urlValidator = new UrlValidator( UrlValidator.ALLOW_ALL_SCHEMES );
         if (urlValidator.isValid( uriIn )) {
             uri = uriIn;
         }
