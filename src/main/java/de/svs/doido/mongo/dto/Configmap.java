5830edc67728990805441fb7240ef99e22fb7b02
mamutmk5 <3045922+mamutmk5@users.noreply.github.com>
2024-05-23 10:29:58 +0200

    BC-7234 - test hashcode

--- src/main/java/de/svs/doido/mongo/dto/Configmap.java
+++ src/main/java/de/svs/doido/mongo/dto/Configmap.java
@@ -3,10 +3,12 @@ package svs.doido.mongo.dto;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
+import lombok.EqualsAndHashCode;
 import org.apache.commons.validator.routines.UrlValidator;
 
 
 @ToString
+@EqualsAndHashCode
 public class Configmap {
     @Getter
     @Setter
