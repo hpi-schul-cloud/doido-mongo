617abfff0a97fb6a05b1532a2de3d5673f77d2b8
mamutmk5 <3045922+mamutmk5@users.noreply.github.com>
2024-05-23 17:39:31 +0200

    BC-7234 - less test

--- src/test/java/de/svs/doido/mongo/dto/ConfigmapTest.java
+++ src/test/java/de/svs/doido/mongo/dto/ConfigmapTest.java
@@ -3,34 +3,17 @@ package svs.doido.mongo.dto;
 import io.quarkus.test.junit.QuarkusTest;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.BeforeEach;
-import lombok.ToString;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.Setter;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 @QuarkusTest
 class ConfigmapTest {
-    @ToString
-    @EqualsAndHashCode(callSuper=true)
-    @Getter
-    @Setter
-    public static class testConfigmapClass extends Configmap {
-        public testConfigmapClass() {
-            super();
-        }
-    }
     
     private Configmap c;
-    private Configmap d;
-    private Configmap sub;
 
     @BeforeEach
     public void before() {
         c = new Configmap();
-        d = new Configmap();
-        sub = new testConfigmapClass();
     }
 
     @Test
@@ -48,159 +31,9 @@ class ConfigmapTest {
     }
 
     @Test
-    void testGetterSetterSubName() {
-        String name = "hausboot";
-        sub.setName(name);
-        assertEquals(name,sub.getName());
-    }
-
-    @Test
-    void testGetterSetterSubUri() {
-        String uri = "proto://foo.bar.com/";
-        sub.setUri(uri);
-        assertEquals(uri,sub.getUri());
-    }
-
-    @Test
     void testGetterSetterUriFail() {
         String uri = "proto:foo.bar.com/";
         c.setUri(uri);
         assertEquals("", c.getUri());
-    }
-
-    @Test
-    void testToString() {
-        String uri = "proto://foo.bar.com/";
-        String name = "hausboot";
-        c.setName(name);
-        c.setUri(uri);
-        d.setName(name);
-        d.setUri(uri);
-        assertEquals(d.toString(), c.toString());
-    }
-
-    @Test
-    void testHashcode() {
-        String uri = "proto://foo.bar.com/";
-        String name = "hausboot";
-        assertEquals(c.hashCode(), c.hashCode());
-        assertNotEquals((new Object()).hashCode(), d.hashCode());
-        c.setName(name);
-        assertNotEquals(d.hashCode(), c.hashCode());
-        c.setUri(uri);
-        assertNotEquals(d.hashCode(), c.hashCode());
-        d.setName(name);
-        assertNotEquals(d.hashCode(), c.hashCode());
-        d.setUri(uri);
-        assertEquals(d.hashCode(), c.hashCode());
-        d.setName(name);
-        d.setUri("mongo://uri.foo.bar.com/?uir=a");
-        assertNotEquals(d.hashCode(), c.hashCode());
-        assertEquals(c.hashCode(), c.hashCode());
-    }
-
-    @Test
-    void testHashcodeSubClass () {
-        String uri = "proto://foo.bar.com/";
-        String name = "hausboot";
-        assertEquals(sub.hashCode(), c.hashCode());
-        c.setName(name);
-        assertNotEquals(sub.hashCode(), c.hashCode());
-        c.setUri(uri);
-        assertNotEquals(sub.hashCode(), c.hashCode());
-        sub.setName(name);
-        assertNotEquals(sub.hashCode(), c.hashCode());
-        sub.setUri(uri);
-        assertEquals(sub.hashCode(), c.hashCode());
-        sub.setName(name);
-        sub.setUri("mongo://uri.foo.bar.com/?uir=a");
-        assertNotEquals(sub.hashCode(), c.hashCode());
-    }
-
-    @Test
-    void testEqualsEmpty () {
-        assertEquals( true, c.equals(c));
-        assertEquals( true, d.equals(c));
-    }
-
-    @Test
-    void testEqualsEmptySub () {
-        assertEquals( false, c.equals(sub));
-        assertEquals( false, sub.equals(c));
-    }
-
-
-    @Test
-    void testEqualsObject () {
-        String uri = "proto://foo.bar.com/";
-        String name = "hausboot";
-        d.setName(name);
-        d.setUri(uri);
-        assertEquals( false, c.equals(new Object()));
-        assertEquals( false, d.equals(new Object()));
-    }
-
-    @Test
-    void testEqualsName () {
-        String uri = "proto://foo.bar.com/";
-        String name = "hausboot";
-        c.setName(name);
-        assertEquals( false, d.equals(c));
-        assertEquals( false, c.equals(d));
-        d.setName(name);
-        assertEquals( true, d.equals(c));
-        assertEquals( true, c.equals(d));
-    }
-
-    @Test
-    void testEqualsSubName () {
-        String uri = "proto://foo.bar.com/";
-        String name = "hausboot";
-        c.setName(name);
-        assertEquals( false, sub.equals(c));
-        assertEquals( false, c.equals(sub));
-        sub.setName(name);
-        assertEquals( false, sub.equals(c));
-        assertEquals( false, c.equals(sub));
-    }
-
-    @Test
-    void testEqualsUri () {
-        String uri = "proto://foo.bar.com/";
-        String name = "hausboot";
-        c.setUri(uri);
-        assertEquals( false, d.equals(c));
-        assertEquals( false, c.equals(d));
-        d.setUri(uri);
-        assertEquals( true, d.equals(c));
-        assertEquals( true, c.equals(d));
-    }
-
-    @Test
-    void testEqualsUriHttps () {
-        String uri = "https://foo.bar.com/";
-        String name = "hausboot";
-        c.setUri(uri);
-        assertEquals( false, d.equals(c));
-        assertEquals( false, c.equals(d));
-        d.setUri(uri);
-        assertEquals( true, d.equals(c));
-        assertEquals( true, c.equals(d));
-    }
-
-    @Test
-    void testEqualsNameUri () {
-        String uri = "proto://foo.bar.com/";
-        String name = "hausboot";
-        c.setUri(uri);
-        c.setName(name);
-        assertEquals( false, d.equals(c));
-        assertEquals( false, c.equals(d));
-        d.setUri(uri);
-        assertEquals( false, d.equals(c));
-        assertEquals( false, c.equals(d));
-        d.setName(name);
-        assertEquals( true, d.equals(c));
-        assertEquals( true, c.equals(d));
     }
 }
