d5483dc6e8bc49f54ad76330dca5d975ed45c37d
mamutmk5 <3045922+mamutmk5@users.noreply.github.com>
2024-06-21 15:38:17 +0200

    BC-7239 - write first draft

--- src/main/java/de/svs/doido/mongo/ConfigmapApi.java
+++ src/main/java/de/svs/doido/mongo/ConfigmapApi.java
@@ -2,22 +2,34 @@ package svs.doido.mongo;
 
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.inject.Inject;
 import svs.doido.mongo.dto.Configmap;
 import svs.doido.mongo.kubernetes.configmap.Read;
+import svs.doido.mongo.kubernetes.configmap.Write;
 
 @Path("/configmap")
 public class ConfigmapApi {
 
     @Inject
-    Read cfg;
+    Read cfgRead;
+    
+    @Inject
+    Write cfgWrite;
 
     @GET
     @Path("/{namespace}/{configmapName}")
     @Produces(MediaType.APPLICATION_JSON)
-    public Configmap configmaps(String namespace, String configmapName) {
-        return cfg.readConfigmap(namespace,configmapName);
+    public Configmap configmapsRead(String namespace, String configmapName) {
+        return cfgRead.readConfigmap(namespace,configmapName);
+    }
+
+    @POST
+    @Path("/{namespace}/{configmapName}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public void configmapsWrite(String namespace, String configmapName) {
+        cfgWrite.writeConfigmap(namespace,configmapName);
     }
 }
---
+++ src/main/java/de/svs/doido/mongo/kubernetes/configmap/Write.java
@@ -0,0 +1,51 @@
+package svs.doido.mongo.kubernetes.configmap;
+
+import svs.doido.mongo.dto.Configmap;
+import io.fabric8.kubernetes.api.model.ConfigMap;
+import io.fabric8.kubernetes.api.model.ConfigMapBuilder;
+import io.fabric8.kubernetes.api.model.ObjectMeta;
+import io.fabric8.kubernetes.client.KubernetesClient;
+import jakarta.inject.Inject;
+import jakarta.enterprise.context.RequestScoped;
+
+import java.util.Map;
+import java.util.HashMap;
+
+@RequestScoped
+public class Write {
+
+    @Inject
+    KubernetesClient client;
+
+    public void writeConfigmap(String namespace, String name, Configmap cfg) {
+        ConfigMap configmap = client.configMaps().inNamespace(namespace).withName(cfg.getName()).get();
+        Map<String,String> labels;
+        Map<String,String> data;
+        ObjectMeta meta;
+
+        if(configmap != NULL) {
+            labels = configmap.getMetadata().getLabels();
+            if( 
+                labels.containsKey("app.kubernetes.io/name") &&
+                labels.get("app.kubernetes.io/name").equal("doido-mongo")
+            ) {
+                data = configmap.getData();
+                data.put("uri", cfg.getUri);
+                configmap.setData(data);
+                client.configMaps().resource(configmap).createOrReplace();
+            }
+        }
+        else {
+            labels = new HashMap<String,String>();
+            data = new HashMap<String,String>();
+            labels.put("app.kubernetes.io/name", "doido-mongo");
+            data.put("uri", cfg.getUri());
+            configmap = new ConfigMapBuilder().withNewMetadata().withName(cfg.getName()).withNamespace(namespace).and().build();
+            configmap.setData(data);
+            meta = configmap.getMetadata();
+            meta.setLables(labels);
+            configmap.setMetadata(meta);
+            client.configMaps().resource(configmap).create();
+        }
+    }
+}
