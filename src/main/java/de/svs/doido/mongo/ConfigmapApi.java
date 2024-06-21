9f894f39d91e1138d2b69210354a93baa5ce5c2f
mamutmk5 <3045922+mamutmk5@users.noreply.github.com>
2024-06-21 11:22:14 +0200

    BC-7234 - reduce sonar complain

--- src/main/java/de/svs/doido/mongo/ConfigmapApi.java
+++ src/main/java/de/svs/doido/mongo/ConfigmapApi.java
@@ -5,7 +5,6 @@ import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 import io.fabric8.kubernetes.client.KubernetesClient;
-import java.util.List;
 import jakarta.inject.Inject;
 import svs.doido.mongo.dto.Configmap;
 import svs.doido.mongo.kubernetes.configmap.Read;
@@ -13,14 +12,8 @@ import svs.doido.mongo.kubernetes.configmap.Read;
 @Path("/configmap")
 public class ConfigmapApi {
 
-    private final KubernetesClient kubernetesClient;
-
     @Inject
     Read cfg;
-
-    public ConfigmapApi(KubernetesClient kubernetesClient) {
-        this.kubernetesClient = kubernetesClient;
-    }
 
     @GET
     @Path("/{namespace}/{configmapName}")
