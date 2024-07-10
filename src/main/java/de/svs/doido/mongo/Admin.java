92941f9e9e581fbeee40bb9703cd3fd7c6adfdea
mamutmk5 <3045922+mamutmk5@users.noreply.github.com>
2024-07-10 15:59:55 +0200

    BC-7240 - next test

--- src/main/java/de/svs/doido/mongo/Admin.java
+++ src/main/java/de/svs/doido/mongo/Admin.java
@@ -5,15 +5,20 @@ import jakarta.ws.rs.GET;
 import jakarta.inject.Inject;
 import io.fabric8.kubernetes.api.model.Namespace;
 import io.fabric8.kubernetes.client.KubernetesClient;
+
+import org.bson.BsonDocument;
+import org.bson.BsonInt64;
+import org.bson.Document;
+import org.bson.conversions.Bson;
+import com.mongodb.*;
 import com.mongodb.client.MongoClient;
-import io.quarkus.mongodb.runtime.MongoClientConfig;
+import com.mongodb.client.MongoClients;
 import java.util.List;
 
 @Path("/admin")
 public class Admin {
 
     private final KubernetesClient kubernetesClient;
-    @Inject MongoClient mongoClient;
     
 
     public Admin(KubernetesClient kubernetesClient) {
@@ -23,7 +28,24 @@ public class Admin {
     @GET
     @Path("/{clientname}")
     public void testConnect(String clientname) {
-        MongoClient monClient = mongoClient.createMongoClient(clientname);
-        MongoClientConfig monClientCfg = mongoClient.getMatchingMongoClientConfig(clientname);
+        String uri = "mongodb://mongodb-2.mongo-svc:27017/replicaSet=rs0&directConnection=false";
+        ServerApi serverApi = ServerApi.builder()
+                .version(ServerApiVersion.V1)
+                .build():
+         MongoClientSettings settings = MongoClientSettings.builder()
+                .applyConnectionString(new ConnectionString(uri))
+                .serverApi(serverApi)
+                .build();
+        try (MongoClient mongoClient = MongoClients.create(settings)) {
+            MongoDatabase database = mongoClient.getDatabase("admin");
+            try {
+                // Send a ping to confirm a successful connection
+                Bson command = new BsonDocument("ping", new BsonInt64(1));
+                Document commandResult = database.runCommand(command);
+                System.out.println("Pinged your deployment. You successfully connected to MongoDB!");
+            } catch (MongoException me) {
+                System.err.println(me);
+            }
+        }
     }
 }
