6ae99fd3aa249a087a07a1f9e5fd73bcff3d6f0a
mamutmk5 <3045922+mamutmk5@users.noreply.github.com>
2024-07-15 10:40:32 +0200

    BC-7240 - test equal methode

---
+++ src/test/java/de/svs/doido/mongo/dto/MongoServerTest.java
@@ -0,0 +1,17 @@
+package svs.doido.mongo.dto;
+
+import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@QuarkusTest
+class MongoServerTest {
+
+    @Test
+    void testGetterSetterName() {
+        MongoServer a = new MongoServer("Server 1","mongodb://mongodb-2.mongo-svc:27017/replicaSet=rs0&directConnection=false");        
+        MongoServer b = new MongoServer("Server 1","mongodb://mongodb-2.mongo-svc:27017/replicaSet=rs0&directConnection=false");
+        assertEquals(1,a.equal(b));
+    }
+}
